da63f35fc4eb8b5804e32e50b6cf701550cf8b42
Isaias Gregory <106548529+Lightgreg@users.noreply.github.com>
2022-06-14 19:24:56 -0300

    4° entrega...

---
+++ scrypt.js
@@ -0,0 +1,10 @@
+let nome = "Isaias"
+let sobrenome = "Gregory"
+let anoAtual = 2022
+let anoNascimento = 1992
+let cidade = "teresina"
+let idade = (anoAtual-anoNascimento)
+
+    console.log("Eu sou o"+" "+nome+" "+sobrenome+" "+"tenho"+" "+idade+" "+"anos"+" "+"e moro em"+" "+cidade) 
+
+
